58c1881132bbf8c1d2d129e1dfd5d4b5a27ec149
fadhilelrizanda <fadhilelrizanda@gmail.com>
2022-06-15 16:05:07 +0700

    first commit7

--- change_txt_dataset.py
+++ change_txt_dataset.py
@@ -2,9 +2,9 @@ import os
 
 with open('chicken/data/chicken/training.txt', 'w') as out:
     for img in [f for f in os.listdir("chicken/data/chicken" + '/train') if f.endswith('jpg')]:
-        out.write('chicken/data/chicken/train' + img + '\n')
+        out.write('chicken/data/chicken/train/' + img + '\n')
 
 
 with open('chicken/data/chicken/valid.txt', 'w') as out:
     for img in [f for f in os.listdir('chicken/data/chicken' + '/valid') if f.endswith('jpg')]:
-        out.write('chicken/data/chicken/valid' + img + '\n')
+        out.write('chicken/data/chicken/valid/' + img + '\n')
